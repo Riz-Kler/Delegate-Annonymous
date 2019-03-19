5257090826d42cebc846fc144abe19a11cd2f877
Rizwan Kler <rizwan.kler@gmail.com>
2019-03-19 03:31:09 +0000

    Code

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication24
+{
+   delegate void Operation(int num);
+
+   // delegate int Operation(int num);
+
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            
+ //            Operation op = Double;
+ //            op(2);
+
+
+
+  //          Operation op = delegate(int num) { Console.WriteLine("{0} x 2 = {1}", num, num * 2);
+
+   //             op(2);
+
+            Operation op=num =>
+            {
+                Console.WriteLine("{0} x 2 = {1}", num, num * 2);     
+
+                // return num *2;
+            };
+          //  int value = op(2);
+  
+          
+            op(2);
+
+           // Console.WriteLine(value);
+           Console.ReadLine();
+
+
+        }
+
+  //      static void Double(int num)
+  //      {
+  
+
+  //      }
+    }
+}
